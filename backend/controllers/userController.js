bda7f1d964989ee67dc240f84111ef18d2a34d08
Carrie <carriestylesf4@gmail.com>
2022-07-23 13:08:14 +0300

    Add authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,94 @@
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+// @desc    Register a new user
+// @route   /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+
+  const { name, email, password } = req.body
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please include all fields')
+  }
+
+  // Find if user already exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: genToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new error('Invalid user data')
+  }
+})
+   
+
+// @desc    Log in a user
+// @route   /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))){
+          if (user) {
+            res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: genToken(user._id)
+            })
+        } else {
+            res.status(401)
+            throw new Error('Invalid email')
+        }
+    }
+})
+const genToken=(id)=>{
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
+// @desc    Get current user info
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+   const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+    }
+    res.status(200).json(user)
+})
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe, 
+}
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { errorHandler } = require('./middleware/errorHandler')
+const { connectDB } = require('./config/db')
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+//connect to db
+connectDB()
+const app = express()
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/',(req, res)=>{
+    res.status(200).json({message:'Welcome to project managment api'})
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
+
+app.listen(PORT,()=> console.log(`Server running at port ${PORT}`))
